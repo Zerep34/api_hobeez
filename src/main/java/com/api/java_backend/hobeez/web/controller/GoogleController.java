ebbdea1a5fb694c29ee1af7901d0fbf0de58827e
romain <romain-hd.perez@live.fr>
2020-04-03 15:54:53 +0200

    ajout api google places

---
+++ src/main/java/com/api/java_backend/hobeez/web/controller/GoogleController.java
@@ -0,0 +1,139 @@
+package com.api.java_backend.hobeez.web.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+@RestController
+@RequestMapping(path="api/hobeez/google")
+public class GoogleController {
+
+    String API_KEY = "AIzaSyCL1grfGNMygaod44vtCJQWZSShgweACY0";
+
+    @GetMapping(path="/nearby")
+    public @ResponseBody String google_nearby (@RequestParam String perimetre,
+                                               @RequestParam String longitude,
+                                               @RequestParam String lattitude,
+                                               @RequestParam String l_type_raw) throws IOException {
+        perimetre = perimetre + "000";
+
+        if(l_type_raw.equals("empty")){
+            String url = String.format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s,%s&radius=%s&key=%s", lattitude, longitude, perimetre, API_KEY);
+            System.out.println(url);
+            URL googleapi = new URL(url);
+            URLConnection yc = googleapi.openConnection();
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            yc.getInputStream()));
+            String inputLine;
+
+            String json = "";
+
+            while ((inputLine = in.readLine()) != null){
+                json += inputLine;
+            }
+            in.close();
+            return json;
+        }
+        else{
+            String result = "";
+            String[] l_type = l_type_raw.split(",");
+            for (String type: l_type) {
+                String url = String.format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s,%s&radius=%s&type=%s&opennow=true&key=%s&opennow=true", lattitude, longitude, perimetre, type, API_KEY);
+                URL googleapi = new URL(url);
+                URLConnection yc = googleapi.openConnection();
+
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(
+                                yc.getInputStream()));
+                String inputLine;
+
+                String json = "";
+
+                while ((inputLine = in.readLine()) != null){
+                    json += inputLine;
+                }
+                in.close();
+                result += json + "--|--";
+            }
+            return result;
+        }
+    }
+
+    @GetMapping(path="/details")
+    public @ResponseBody String google_details (@RequestParam String place_id) throws IOException {
+        String url = String.format("https://maps.googleapis.com/maps/api/place/details/json?place_id=%s&fields=address_component,adr_address,formatted_address,geometry,icon,name,plus_code,type,formatted_phone_number,opening_hours,website,price_level,rating&key=%s", place_id, API_KEY);
+        URL googleapi = new URL(url);
+        URLConnection yc = googleapi.openConnection();
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        yc.getInputStream()));
+        String inputLine;
+
+        String json = "";
+
+        while ((inputLine = in.readLine()) != null){
+            json += inputLine;
+        }
+        in.close();
+        return json;
+    }
+
+    @GetMapping(path="/refresh")
+    public @ResponseBody String google_details (@RequestParam String perimetre,
+                                                @RequestParam String longitude,
+                                                @RequestParam String lattitude,
+                                                @RequestParam String l_type_raw,
+                                                @RequestParam String refresh_token) throws IOException {
+        perimetre = perimetre + "000";
+
+        if(l_type_raw.equals("empty")){
+            String url = String.format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s,%s&radius=%s&opennow=true&key=%s", lattitude, longitude, perimetre, API_KEY, refresh_token);
+            URL googleapi = new URL(url);
+            URLConnection yc = googleapi.openConnection();
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            yc.getInputStream()));
+            String inputLine;
+
+            String json = "";
+
+            while ((inputLine = in.readLine()) != null){
+                json += inputLine;
+            }
+            in.close();
+            return json;
+        }
+        else{
+            String result = "";
+            String[] l_type = l_type_raw.split(",");
+            for (String type: l_type) {
+                String url = String.format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s,%s&radius=%s&type=%s&opennow=true&key=%s&pagetoken=%s", lattitude, longitude, perimetre, type, API_KEY);
+                URL googleapi = new URL(url);
+                URLConnection yc = googleapi.openConnection();
+
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(
+                                yc.getInputStream()));
+                String inputLine;
+
+                String json = "";
+
+                while ((inputLine = in.readLine()) != null){
+                    json += inputLine;
+                }
+                in.close();
+                result += json + "--|--";
+            }
+            return result;
+        }
+    }
+}
+
--- src/main/java/com/api/java_backend/hobeez/web/controller/UtilisateurController.java
+++ src/main/java/com/api/java_backend/hobeez/web/controller/UtilisateurController.java
@@ -3,9 +3,22 @@ package com.api.java_backend.hobeez.web.controller;
 import com.api.java_backend.hobeez.model.Utilisateurs;
 import com.api.java_backend.hobeez.repository.UserRepository;
 import com.api.java_backend.hobeez.repository.service.UserService;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -14,6 +27,8 @@ import java.util.Date;
 @RestController
 @RequestMapping(path="api/hobeez/user")
 public class UtilisateurController {
+
+    String API_KEY = "AIzaSyCL1grfGNMygaod44vtCJQWZSShgweACY0";
 
     @Autowired
     private UserRepository userRepository;
