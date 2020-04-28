855eb300426bd2bb61e815a69b066c0ec6a0213f
Mona Lisa <romain-hd.perez@live.fr>
2020-04-28 13:56:21 +0200

    add Jenkinsfile

--- source.sql
+++ source.sql
@@ -12,10 +12,10 @@ CREATE TABLE `Utilisateurs`
 );
 
 Insert Into Utilisateurs(email, pass, full_name, birthday, hasChild)
-Values ("romain@mail.com", "$2y$10$CNjuXv5DrS3PeeeRJNiGV.Yzx8aYhP3TvLIo4P/M0OWemmgTZ62s2", "romain perez",  CAST("1999-09-15" AS date), false);
+Values ("romain@mail.com", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "romain perez",  CAST("1999-09-15" AS date), false);
 Insert Into Utilisateurs(email, pass, full_name, birthday, hasChild)
-Values ("antoine@mail.com", "$2y$10$CNjuXv5DrS3PeeeRJNiGV.Yzx8aYhP3TvLIo4P/M0OWemmgTZ62s2", "antoine guenard",  CAST("1999-03-22" AS date), false);
+Values ("antoine@mail.com", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "antoine guenard",  CAST("1999-03-22" AS date), false);
 Insert Into Utilisateurs(email, pass, full_name, birthday, hasChild)
-Values ("camille@mail.com", "$2y$10$CNjuXv5DrS3PeeeRJNiGV.Yzx8aYhP3TvLIo4P/M0OWemmgTZ62s2", "camille vidal",  CAST("1993-06-15" AS date), true);
+Values ("camille@mail.com", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "camille vidal",  CAST("1993-06-15" AS date), true);
 Insert Into Utilisateurs(email, pass, full_name, birthday, hasChild)
-Values ("lucas@mail.com", "$2y$10$CNjuXv5DrS3PeeeRJNiGV.Yzx8aYhP3TvLIo4P/M0OWemmgTZ62s2", "lucas gayraud",  CAST("1999-08-23" AS date), false);
+Values ("lucas@mail.com", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "lucas gayraud",  CAST("1999-08-23" AS date), false);
