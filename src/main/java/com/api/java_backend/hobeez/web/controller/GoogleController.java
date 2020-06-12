2843530f256f178edab56a086db9cdcd9ad4871a
romain <romain-hd.perez@live.fr>
2020-06-12 15:57:54 +0200

    add api to find gps from city

--- src/main/java/com/api/java_backend/hobeez/web/controller/GoogleController.java
+++ src/main/java/com/api/java_backend/hobeez/web/controller/GoogleController.java
@@ -1,17 +1,23 @@
 package com.api.java_backend.hobeez.web.controller;
 
 import com.api.java_backend.hobeez.util.JsonParserUtil;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.hibernate.validator.internal.util.privilegedactions.GetMethod;
+import org.springframework.http.HttpRequest;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import sun.misc.IOUtils;
+import sun.net.www.http.HttpClient;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
+import java.net.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 @RestController
 @RequestMapping(path="api/hobeez/google")
@@ -153,10 +159,12 @@ public class GoogleController {
     @PostMapping(path="/ghome")
     @PreAuthorize("hasRole('GHOME')")
     public @ResponseBody String google_home (@RequestParam String perimetre,
-                                                @RequestParam String longitude,
-                                                @RequestParam String lattitude,
+                                                @RequestParam String ville,
                                                 @RequestParam String categorie) throws IOException {
         perimetre = perimetre + "000";
+        String[] coord = this.getCoordinateFromCity(ville);
+        String lattitude = coord[1];
+        String longitude = coord[0];
 
         if(categorie.equals("empty")){ // TODO a voir avec Antoine
             String url = String.format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%s,%s&radius=%s&opennow=true&key=%s", lattitude, longitude, perimetre, API_KEY);
@@ -198,5 +206,20 @@ public class GoogleController {
             return jsonParser.createJson(json).toString();
         }
     }
+
+    public String[] getCoordinateFromCity(String city) throws IOException {
+        String urlString = "https://api-adresse.data.gouv.fr/search/?q="+city;
+        URL url = new URL(urlString);
+        URLConnection conn = url.openConnection();
+        InputStream is = conn.getInputStream();
+        Scanner s = new Scanner(is).useDelimiter("\\A");
+        String result = s.hasNext() ? s.next() : "";
+        JsonParser parser = new com.google.gson.JsonParser();
+        String json_features = parser.parse(result).getAsJsonObject().get("features").toString();
+        String coordinates = json_features.substring(json_features.indexOf("coordinates")+14, json_features.indexOf("]"));
+        return coordinates.split(",");
+    }
+
+
 }
 
